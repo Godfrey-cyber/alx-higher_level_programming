bd6b9dcbf0edced7b07cadbd517454684848c9fe
New Name <godfreyndiritu26@gmail.com>
2024-09-10 12:01:21 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
+for x in range(10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
             print('98')
         else:
             print('{}{}, '.format(x, y), end='')
