8624187a209f1a8c582ff60d502b3fbe844da298
New Name <godfreyndiritu26@gmail.com>
2024-09-16 08:53:43 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    # Use list comprehension to exclude 'c' and 'C', then join the list back into a string
-    new_string = ''.join([char for char in my_string if char != 'c' and char != 'C'])
-    return new_string
+    updated_str = ''
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            updated_str += i
+    return (updated_str)
